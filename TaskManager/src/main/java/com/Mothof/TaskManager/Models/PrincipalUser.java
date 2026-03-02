65d9548d6657043a80c2fc6c91226c8a00607d73
Tshepo Majoro <mothofeelama@gmail.com>
2026-03-02 12:50:26 +0200

    added the PrincipalUser class which implements the UserDetails interface, this we need in the MyUserDetailsService class' loadUserByUsername() method

---
+++ TaskManager/src/main/java/com/Mothof/TaskManager/Models/PrincipalUser.java
@@ -0,0 +1,33 @@
+package com.Mothof.TaskManager.Models;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class PrincipalUser implements UserDetails {
+
+    private Users user;
+
+    public PrincipalUser(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(GrantedAuthority.class.cast("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
