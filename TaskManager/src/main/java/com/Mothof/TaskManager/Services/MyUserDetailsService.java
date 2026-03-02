ead6fb6e0a0a8e471e9c59381b8e450ea2890bb9
Tshepo Majoro <mothofeelama@gmail.com>
2026-03-02 13:00:27 +0200

    Wrote code implementation for the loadUSerByUsername() method

--- TaskManager/src/main/java/com/Mothof/TaskManager/Services/MyUserDetailsService.java
+++ TaskManager/src/main/java/com/Mothof/TaskManager/Services/MyUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.Mothof.TaskManager.Services;
 
+import com.Mothof.TaskManager.Models.PrincipalUser;
+import com.Mothof.TaskManager.Models.Users;
+import com.Mothof.TaskManager.Repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Users user = usersRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new PrincipalUser(user);
     }
 }
