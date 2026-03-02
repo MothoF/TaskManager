8119713aceaeea5bea0818595f2943d72db2e4ab
Tshepo Majoro <mothofeelama@gmail.com>
2026-03-02 12:33:23 +0200

    fixed UsersRepository and made it an interface as it should be, I also added a custom findByUsername() method to it

--- TaskManager/src/main/java/com/Mothof/TaskManager/Repository/UsersRepository.java
+++ TaskManager/src/main/java/com/Mothof/TaskManager/Repository/UsersRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public abstract class UserRepository implements JpaRepository<Users, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+    Users findByUsername(String username);
 }
