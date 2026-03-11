4c23da9621b447a217d851e3078cdbf8963fb0b3
Tshepo Majoro <mothofeelama@gmail.com>
2026-03-11 16:56:23 +0200

    added a method to the SecurityConfigurations file for the AuthenticationManager bean

--- TaskManager/src/main/java/com/Mothof/TaskManager/Configurations/SecurityConfigurations.java
+++ TaskManager/src/main/java/com/Mothof/TaskManager/Configurations/SecurityConfigurations.java
@@ -3,8 +3,10 @@ package com.Mothof.TaskManager.Configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -44,5 +46,10 @@ public class SecurityConfigurations {
         daoAuthProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 //        daoAuthProvider.setUserDetailsService(userDetailsService);
         return daoAuthProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
