1f9d9b53755c85ada941b4dec210c3b8315a267f
Tshepo Majoro <mothofeelama@gmail.com>
2026-03-11 17:42:28 +0200

    added jjwt api and jjwt impl dependencies to the pom file. Also created a JwtService file which creates a token in the generateToken() method and a secret key in the getSecretKey() method

---
+++ TaskManager/src/main/java/com/Mothof/TaskManager/Services/JwtService.java
@@ -0,0 +1,60 @@
+package com.Mothof.TaskManager.Services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.core.PropagationContextElement.Key;
+
+@Service
+public class JwtService {
+    private String secretKey;
+
+    public JwtService() {
+        try{
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey key = keyGenerator.generateKey();
+            this.secretKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    This method generate a JavaScript Web Token
+    Parameter String username -> represents the username of the user trying to login
+
+    The method then adds to the token the:
+    - subject (i.e. username)
+    - issue date
+    - expiration date (i.e. issue date + 30 minutes)
+    - a unique signature (i.e. secret key)
+     */
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1800000))
+                .and()
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] encodedKey = Base64.getDecoder().decode(secretKey);
+        SecretKey key = Keys.hmacShaKeyFor(encodedKey);
+        return key;
+    }
+}
